9d66e4146ade1e0539be6d048b447c02b9344e62
Michael <Michael@STRONGBAD>
2017-01-21 20:32:06 -0500

    Add project files.

---
+++ Running_Timer/Running_Timer.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Running_Timer.ino
+ Created:	1/21/2017 8:31:54 PM
+ Author:	Michael
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
